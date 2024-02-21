6ef7b7484279e5cc8db6a43d76edd7d52dddcc41
matipt2 <mateuszpt2@o2.pl>
2024-02-21 20:21:41 +0100

    delete some files

--- cw9/library.h
+++
@@ -1,8 +0,0 @@
-#include <pthread.h>
-
-void unlocking(pthread_mutex_t *mutex);
-pthread_t createThread(void *function(void *), void *argument);
-void initialization(pthread_mutex_t *mutex, const pthread_mutexattr_t *attr);
-void waitForThread(pthread_t thread, void **status);
-void locking(pthread_mutex_t *mutex);
-void destruction(pthread_mutex_t *mutex);
